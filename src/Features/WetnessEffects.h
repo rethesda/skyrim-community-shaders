3895ba5ffa8caa046b973ec01512ad716ff06513
Skrubby Skrub In A Shrub <87662196+SkrubbySkrubInAShrub@users.noreply.github.com>
2026-03-25 03:45:47 +0100

    chore(wetness-effects): set default MaxPuddleWetness to 1.5 (#1981)
    Co-authored-by: SkrubbySkrubInAShrub <skrubbyskrubinashrub@gmail.com>

--- src/Features/WetnessEffects.h
+++ src/Features/WetnessEffects.h
@@ -32,7 +32,7 @@ public:
 	{
 		uint EnableWetnessEffects = true;
 		float MaxRainWetness = 1.0f;
-		float MaxPuddleWetness = 2.5f;
+		float MaxPuddleWetness = 1.5f;
 		float MaxShoreWetness = 1.0f;
 		uint ShoreRange = 32;
 		float PuddleRadius = 1.0f;
