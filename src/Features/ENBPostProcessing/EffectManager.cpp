d8ac6ff3ed9114568dca218c9d489a0f67635df4
doodlum <15017472+doodlum@users.noreply.github.com>
2026-04-02 00:23:42 +0100

    fix: Bounds-check the compute shader dispatch.

--- src/Features/ENBPostProcessing/EffectManager.cpp
+++ src/Features/ENBPostProcessing/EffectManager.cpp
@@ -462,6 +462,12 @@ void EffectManager::CreateColorCorrectionShader()
 		[numthreads(8, 8, 1)]
 		void main(uint3 id : SV_DispatchThreadID)
 		{
+			uint width, height;
+			OutputTexture.GetDimensions(width, height);
+			if (id.x >= width || id.y >= height) {
+				return;
+			}
+
 			float4 color = OutputTexture[id.xy];
 			color.rgb = pow(color.rgb, GammaCurve);
 			color.rgb *= Brightness;
