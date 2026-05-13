5080275356b978f6b6f87fcfe8dc88ad47e1aa58
soda <130315225+soda3000@users.noreply.github.com>
2026-05-13 12:18:12 -0300

    fix(truepbr): remove EnvmapData breaking MATOs (#2336)

--- package/Shaders/Lighting.hlsl
+++ package/Shaders/Lighting.hlsl
@@ -2067,7 +2067,6 @@ PS_OUTPUT main(PS_INPUT input, bool frontFace : SV_IsFrontFace)
 		projectedMaterialWeight = smoothstep(0, 1, 5 * (0.1 + projWeight));
 #			if defined(TRUE_PBR)
 		projBaseColor = max(0, projBaseColor.xyz * MaterialObjectRGBScale);
-		projBaseColor = max(0, EnvmapData.x * projBaseColor);
 		rawRMAOS.xyw = lerp(rawRMAOS.xyw, float3(ParallaxOccData.x, 0, ParallaxOccData.y), projectedMaterialWeight);
 		float4 projectedGlintParameters = 0;
 		if ((PBRFlags & PBR::Flags::ProjectedGlint) != 0) {
